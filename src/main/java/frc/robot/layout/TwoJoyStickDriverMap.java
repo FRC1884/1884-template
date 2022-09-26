0088fd830e4aad4c379e46ed0f21b601ab1a250d
omagarwal25 <lidagarwal@gmail.com>
2022-09-26 23:08:49 +0100

    fixed the field oriented, sorry lads

--- src/main/java/frc/robot/layout/TwoJoyStickDriverMap.java
+++ src/main/java/frc/robot/layout/TwoJoyStickDriverMap.java
@@ -13,11 +13,14 @@ public class TwoJoyStickDriverMap extends DriverMap {
 
   @Override
   public ChassisSpeeds getChassisSpeeds() {
+    // remember that getAxis already normalizes everything
     var x = -controller.getAxis(Axis.AXIS_LEFT_X) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND;
     var y = -controller.getAxis(Axis.AXIS_LEFT_Y) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND;
     var rot = -controller.getAxis(Axis.AXIS_RIGHT_X) * SwerveDrive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
 
-    return new ChassisSpeeds(x, y, rot);
+    var swerve = SwerveDrive.getInstance();
+
+    return ChassisSpeeds.fromFieldRelativeSpeeds(x, y, rot, swerve.getGyroscopeRotation());
   }
 
   @Override
