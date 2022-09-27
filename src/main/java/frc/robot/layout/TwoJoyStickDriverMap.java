8d7b3aee0d7582655026c8d0b78a01ae90587709
omagarwal25 <lidagarwal@gmail.com>
2022-09-27 09:54:08 +0100

    fixed issues

--- src/main/java/frc/robot/layout/TwoJoyStickDriverMap.java
+++ src/main/java/frc/robot/layout/TwoJoyStickDriverMap.java
@@ -13,17 +13,14 @@ public class TwoJoyStickDriverMap extends DriverMap {
 
   @Override
   public ChassisSpeeds getChassisSpeeds() {
-    var y =
-        controller.getAxis(Axis.AXIS_LEFT_X) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND * 0.15;
-    var x =
-        controller.getAxis(Axis.AXIS_LEFT_Y) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND * 0.15;
-    var rot =
-        controller.getAxis(Axis.AXIS_RIGHT_X)
-            * SwerveDrive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
-            * 0.1;
+    var y = controller.getAxis(Axis.AXIS_LEFT_X) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND * 0.15;
+    var x = controller.getAxis(Axis.AXIS_LEFT_Y) * SwerveDrive.MAX_VELOCITY_METERS_PER_SECOND * 0.15;
+    var rot = controller.getAxis(Axis.AXIS_RIGHT_X)
+        * SwerveDrive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+        * 0.1;
 
-    return new ChassisSpeeds(-x, -y, -rot, swerve.getGyroscopeRotation());
     var swerve = SwerveDrive.getInstance();
+    return ChassisSpeeds.fromFieldRelativeSpeeds(-x, -y, -rot, swerve.getGyroscopeRotation());
   }
 
   @Override
