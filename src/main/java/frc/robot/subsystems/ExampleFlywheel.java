1c440d167e7d9a852256ae50a5e02a75c762a816
Edward <55427391+UnknownPaws@users.noreply.github.com>
2022-11-23 00:51:08 +0000

    Vision subsystem, WIP

--- src/main/java/frc/robot/subsystems/ExampleFlywheel.java
+++ src/main/java/frc/robot/subsystems/ExampleFlywheel.java
@@ -5,7 +5,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
-import org.photonvision.PhotonCamera;
 
 public class ExampleFlywheel extends SubsystemBase {
   private static ExampleFlywheel instance;
@@ -18,33 +17,43 @@ public class ExampleFlywheel extends SubsystemBase {
   // Motor Controllers
   private CANSparkMax leaderFlywheel, followerFlywheel;
 
-  // Camera
-  private PhotonCamera vision;
-
   // PID Controllers and Gains
   // PID can be tuned in REV Hardware client
   private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
-  private SparkMaxPIDController leader_pidController;
-  private SparkMaxPIDController follower_pidController;
+  private SparkMaxPIDController leader_pidController, follower_pidController;
 
   private ExampleFlywheel() {
-    leaderFlywheel = new CANSparkMax(RobotMap.FlywheelMap.LEADER_FLYWHEEL, MotorType.kBrushless);
+    leaderFlywheel = 
+        new CANSparkMax(RobotMap.FlywheelMap.LEADER_FLYWHEEL, MotorType.kBrushless);
     followerFlywheel =
         new CANSparkMax(RobotMap.FlywheelMap.FOLLOWER_FLYWHEEL, MotorType.kBrushless);
-    vision = new PhotonCamera(RobotMap.CameraMap.COMPUTER_VISION);
 
-    leaderFlywheel.setInverted(
-        false); // In case one of the motors needs to be invrted before setting them to follow
+    // This method is in case one of the motors needs to be invrted before setting them to follow
+    leaderFlywheel.setInverted(false);
     followerFlywheel.follow(leaderFlywheel, false);
 
-    // leader_pidController = ;
-    // follower_pidController = ;
+    leader_pidController = leaderFlywheel.getPIDController();
+    follower_pidController = followerFlywheel.getPIDController();
     kP = 1;
     kI = 0;
     kD = 0;
     kIz = 0;
     kFF = 0;
-    kMaxOutput = 0;
     kMinOutput = 0;
+    kMaxOutput = 0;
+
+    leader_pidController.setP(kP);
+    leader_pidController.setI(kI);
+    leader_pidController.setD(kD);
+    // leader_pidController.setIZone(kIz);
+    // leader_pidController.setFF(kFF);
+    // leader_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    follower_pidController.setI(kI);
+    follower_pidController.setP(kP);
+    follower_pidController.setD(kD);
+    // follower_pidController.setIZone(kIz);
+    // follower_pidController.setFF(kFF);
+    // follower_pidController.setOutputRange(kMinOutput, kMaxOutput);
   }
 }
