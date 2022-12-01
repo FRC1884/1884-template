c53a2205f4ab0a7cae0940bb3fb57f4b51af36fd
omagarwal25 <lidagarwal@gmail.com>
2022-12-01 01:11:25 +0000

    swerve code module generic now

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,88 +1,116 @@
 package frc.robot;
 
+import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.ModuleConfiguration;
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+
 public class RobotMap {
-  public static class ElevatorMap {
-    public static final int master = 4;
-    public static final int slave = 5;
+    public static class ElevatorMap {
+        public static final int master = 4;
+        public static final int slave = 5;
 
-    public static final int limitSwitch = 0;
-  }
+        public static final int limitSwitch = 0;
+    }
 
-  public static class DriveMap {
-    public static final double DRIVETRAIN_TRACKWIDTH_METERS =
-        0.617; // FIXME Measure and set trackwidth
-    /**
-     * The front-to-back distance between the drivetrain wheels.
-     *
-     * <p>Should be measured from center to center.
-     */
-    public static final double DRIVETRAIN_WHEELBASE_METERS =
-        0.617; // FIXME Measure and set wheelbase
+    public static class DriveMap {
 
-    public static final int DRIVETRAIN_PIGEON_ID = 9; // FIXME Set Pigeon ID
+        public static final double DRIVE_MOTOR_FREE_SPEED = 6380;
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR =
-        7; // FIXME Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR =
-        8; // FIXME Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER =
-        18; // FIXME Set front left steer encoder ID
+        public static class Module {
+            public Module(int driveId, int steerId, int encoderId, double steerOffsetDegrees) {
+                this.driveId = driveId;
+                this.steerId = steerId;
+                this.encoderId = encoderId;
+                this.steerOffsetDegrees = steerOffsetDegrees;
 
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET =
-        -Math.toRadians(270); // FIXME Measure and set front
-    // left steer offset
+            }
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR =
-        1; // FIXME Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR =
-        2; // FIXME Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER =
-        17; // FIXME Set front right steer encoder ID
+            private int driveId;
 
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET =
-        -Math.toRadians(260); // FIXME Measure and set front
-    // right steer offset
+            public int getDriveId() {
+                return driveId;
+            }
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; // FIXME Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; // FIXME Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER =
-        16; // FIXME Set back left steer encoder ID
+            private int steerId;
 
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET =
-        -Math.toRadians(270); // FIXME Measure and set back left
-    // steer offset
+            public int getSteerId() {
+                return steerId;
+            }
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR =
-        3; // FIXME Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR =
-        4; // FIXME Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER =
-        19; // FIXME Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET =
-        -Math.toRadians(265); // FIXME Measure and set back
-    // right steer offset
-  }
+            private int encoderId;
 
-  public static class TankDriveMap {
-    public static final int leftFrontMaster = 0;
-    public static final int leftBackMaster = 1;
-    public static final int rightFrontMaster = 2;
-    public static final int rightBackMaster = 3;
-  }
+            public int getEncoderId() {
+                return encoderId;
+            }
 
-  public static class FlywheelMap {
-    public static final int LEADER_FLYWHEEL = -1; // FIXME Set flywheel motor ID
-    public static final int FOLLOWER_FLYWHEEL = -1; // FIXME Set flywheel motor ID
-  }
+            private double steerOffsetDegrees;
 
-  public static class CameraMap {
-    // Rename the cameras in phtonvision dashboard to the corresponding camera name
-    public static final String COMPUTER_VISION = "camscanner";
-    public static final String DRIVER_CAMERA = "drivercam";
-  }
+            public double getSteerOffset() {
+                return -Math.toRadians(steerOffsetDegrees);
+            }
+        }
 
-  public static class ControllerMap {
-    public static final int DRIVER_JOYSTICK = 0;
-    public static final int OPERATOR_JOYSTICK = 1;
-  }
+        public enum Version {
+            MK3, MK4
+        }
+
+        public static final Version VERSION = Version.MK4;
+
+        public static final ModuleConfiguration MK3_MODULE_CONFIGURATION = SdsModuleConfigurations.MK3_STANDARD;
+        public static final ModuleConfiguration MK4_MODULE_CONFIGURATION = SdsModuleConfigurations.MK4_L1;
+
+        public static ModuleConfiguration getModuleConfiguration() {
+            return VERSION == Version.MK3 ? MK3_MODULE_CONFIGURATION : MK4_MODULE_CONFIGURATION;
+        }
+
+        public static final Mk3SwerveModuleHelper.GearRatio MK3_GEAR_RATIO = Mk3SwerveModuleHelper.GearRatio.STANDARD;
+        public static final Mk4SwerveModuleHelper.GearRatio MK4_GEAR_RATIO = Mk4SwerveModuleHelper.GearRatio.L1;
+
+        public static final double TRACKWIDTH_METERS = 0.617; // FIXME Measure and set trackwidth
+        /**
+         * The front-to-back distance between the drivetrain wheels.
+         *
+         * <p>
+         * Should be measured from center to center.
+         */
+        public static final double WHEELBASE_METERS = 0.617; // FIXME Measure and set wheelbase
+
+        public static final int PIGEON_ID = 9;
+
+        public static final Module FRONT_LEFT_MODULE = new Module(7,
+                8, 18, 270);
+
+        public static final Module FRONT_RIGHT_MODULE = new Module(1,
+                2, 17, 260);
+
+        public static final Module BACK_LEF_MODULE = new Module(5,
+                6, 16, 270);
+
+        public static final Module BACK_RIGHT_MODULE = new Module(3,
+                4, 19, 265);
+    }
+
+    public static class TankDriveMap {
+        public static final int leftFrontMaster = 0;
+        public static final int leftBackMaster = 1;
+        public static final int rightFrontMaster = 2;
+        public static final int rightBackMaster = 3;
+    }
+
+    public static class FlywheelMap {
+        public static final int LEADER_FLYWHEEL = -1; // FIXME Set flywheel motor ID
+        public static final int FOLLOWER_FLYWHEEL = -1; // FIXME Set flywheel motor ID
+    }
+
+    public static class CameraMap {
+        // Rename the cameras in phtonvision dashboard to the corresponding camera name
+        public static final String COMPUTER_VISION = "camscanner";
+        public static final String DRIVER_CAMERA = "drivercam";
+    }
+
+    public static class ControllerMap {
+        public static final int DRIVER_JOYSTICK = 0;
+        public static final int OPERATOR_JOYSTICK = 1;
+    }
 }
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -1,35 +1,19 @@
 package frc.robot.subsystems;
 
-import static frc.robot.RobotMap.DriveMap.BACK_LEFT_MODULE_DRIVE_MOTOR;
-import static frc.robot.RobotMap.DriveMap.BACK_LEFT_MODULE_STEER_ENCODER;
-import static frc.robot.RobotMap.DriveMap.BACK_LEFT_MODULE_STEER_MOTOR;
-import static frc.robot.RobotMap.DriveMap.BACK_LEFT_MODULE_STEER_OFFSET;
-import static frc.robot.RobotMap.DriveMap.BACK_RIGHT_MODULE_DRIVE_MOTOR;
-import static frc.robot.RobotMap.DriveMap.BACK_RIGHT_MODULE_STEER_ENCODER;
-import static frc.robot.RobotMap.DriveMap.BACK_RIGHT_MODULE_STEER_MOTOR;
-import static frc.robot.RobotMap.DriveMap.BACK_RIGHT_MODULE_STEER_OFFSET;
-import static frc.robot.RobotMap.DriveMap.DRIVETRAIN_PIGEON_ID;
-import static frc.robot.RobotMap.DriveMap.DRIVETRAIN_TRACKWIDTH_METERS;
-import static frc.robot.RobotMap.DriveMap.DRIVETRAIN_WHEELBASE_METERS;
-import static frc.robot.RobotMap.DriveMap.FRONT_LEFT_MODULE_DRIVE_MOTOR;
-import static frc.robot.RobotMap.DriveMap.FRONT_LEFT_MODULE_STEER_ENCODER;
-import static frc.robot.RobotMap.DriveMap.FRONT_LEFT_MODULE_STEER_MOTOR;
-import static frc.robot.RobotMap.DriveMap.FRONT_LEFT_MODULE_STEER_OFFSET;
-import static frc.robot.RobotMap.DriveMap.FRONT_RIGHT_MODULE_DRIVE_MOTOR;
-import static frc.robot.RobotMap.DriveMap.FRONT_RIGHT_MODULE_STEER_ENCODER;
-import static frc.robot.RobotMap.DriveMap.FRONT_RIGHT_MODULE_STEER_MOTOR;
-import static frc.robot.RobotMap.DriveMap.FRONT_RIGHT_MODULE_STEER_OFFSET;
+import java.util.HashMap;
+import java.util.function.Supplier;
 
 import com.ctre.phoenix.sensors.WPI_Pigeon2;
 // import com.omagarwal25.swervelib.Mk4SwerveModuleHelper;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 // import com.omagarwal25.swervelib.SdsModuleConfigurations;
 // import com.omagarwal25.swervelib.SwerveModule;
 import com.swervedrivespecialties.swervelib.SwerveModule;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -38,7 +22,6 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -48,22 +31,25 @@ import edu.wpi.first.wpilibj2.command.PerpetualCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.HashMap;
-import java.util.function.Supplier;
+import frc.robot.RobotMap.DriveMap;
+import frc.robot.RobotMap.DriveMap.Version;
 
 public class SwerveDrive extends SubsystemBase {
 
   private static SwerveDrive instance;
 
   public static SwerveDrive getInstance() {
-    if (instance == null) instance = new SwerveDrive();
+    if (instance == null)
+      instance = new SwerveDrive();
     return instance;
   }
 
   /**
    * The maximum voltage that will be delivered to the drive motors.
    *
-   * <p>This can be reduced to cap the robot's maximum speed. Typically, this is useful during
+   * <p>
+   * This can be reduced to cap the robot's maximum speed. Typically, this is
+   * useful during
    * initial testing of the robot.
    */
   public static final double MAX_VOLTAGE = 12.0;
@@ -79,43 +65,42 @@ public class SwerveDrive extends SubsystemBase {
   /**
    * The maximum velocity of the robot in meters per second.
    *
-   * <p>This is a measure of how fast the robot should be able to drive in a straight line.
+   * <p>
+   * This is a measure of how fast the robot should be able to drive in a straight
+   * line.
    */
-  public static final double MAX_VELOCITY_METERS_PER_SECOND =
-      6380.0
-          / 60.0
-          * SdsModuleConfigurations.MK4_L1.getDriveReduction()
-          * SdsModuleConfigurations.MK4_L1.getWheelDiameter()
-          * Math.PI;
+  public static final double MAX_VELOCITY_METERS_PER_SECOND = DriveMap.DRIVE_MOTOR_FREE_SPEED
+      / 60.0
+      * DriveMap.getModuleConfiguration().getDriveReduction()
+      * DriveMap.getModuleConfiguration().getWheelDiameter()
+      * Math.PI;
 
   /**
    * The maximum angular velocity of the robot in radians per second.
    *
-   * <p>This is a measure of how fast the robot can rotate in place.
+   * <p>
+   * This is a measure of how fast the robot can rotate in place.
    */
   // Here we calculate the theoretical maximum angular velocity. You can also
   // replace this with a measured amount.
-  public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND =
-      MAX_VELOCITY_METERS_PER_SECOND
-          / Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
+  public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND
+      / Math.hypot(DriveMap.TRACKWIDTH_METERS / 2.0, DriveMap.WHEELBASE_METERS / 2.0);
 
-  private final SwerveDriveKinematics kinematics =
-      new SwerveDriveKinematics(
-          // Front left
-          new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
-          // Front right
-          new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0),
-          // Back left
-          new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
-          // Back right
-          new Translation2d(
-              -DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0));
+  private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+      // Front left
+      new Translation2d(DriveMap.TRACKWIDTH_METERS / 2.0, DriveMap.WHEELBASE_METERS / 2.0),
+      // Front right
+      new Translation2d(DriveMap.TRACKWIDTH_METERS / 2.0, -DriveMap.WHEELBASE_METERS / 2.0),
+      // Back left
+      new Translation2d(-DriveMap.TRACKWIDTH_METERS / 2.0, DriveMap.WHEELBASE_METERS / 2.0),
+      // Back right
+      new Translation2d(
+          -DriveMap.TRACKWIDTH_METERS / 2.0, -DriveMap.WHEELBASE_METERS / 2.0));
 
   // The important thing about how you configure your gyroscope is that rotating
   // the robot counter-clockwise should
   // cause the angle reading to increase until it wraps back over to zero.
-  // FIXME Remove if you are using a Pigeon
-  private final WPI_Pigeon2 pigeon = new WPI_Pigeon2(DRIVETRAIN_PIGEON_ID);
+  private final WPI_Pigeon2 pigeon = new WPI_Pigeon2(DriveMap.PIGEON_ID);
 
   // These are our modules. We initialize them in the constructor.
   private final SwerveModule frontLeftModule;
@@ -128,60 +113,35 @@ public class SwerveDrive extends SubsystemBase {
   private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
   private SwerveDriveOdometry swerveDriveOdometry;
 
+  private SwerveModule createModule(DriveMap.Module module, ShuffleboardTab tab, int shuffleboardColumn,
+      String name) {
+    var drive = module.getDriveId();
+    var steer = module.getSteerId();
+    var encoder = module.getEncoderId();
+    var offset = module.getSteerOffset();
+
+    var layout = tab.getLayout(name).withSize(2, 4).withPosition(shuffleboardColumn, 0);
+
+    if (DriveMap.VERSION == Version.MK3) {
+      return Mk3SwerveModuleHelper.createFalcon500(
+          layout,
+          DriveMap.MK3_GEAR_RATIO,
+          drive,
+          steer,
+          encoder, offset);
+    } else {
+      return Mk4SwerveModuleHelper.createFalcon500(
+          layout, DriveMap.MK4_GEAR_RATIO, drive, steer, encoder, offset);
+    }
+
+  }
+
   private SwerveDrive() {
     ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
-    frontLeftModule =
-        Mk4SwerveModuleHelper.createFalcon500(
-            // This parameter is optional, but will allow you to see the current state of
-            // the module on the dashboard.
-            tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-                .withSize(2, 4)
-                .withPosition(0, 0),
-            // This can either be STANDARD or FAST depending on your gear configuration
-            Mk4SwerveModuleHelper.GearRatio.L1,
-            // This is the ID of the drive motor
-            FRONT_LEFT_MODULE_DRIVE_MOTOR,
-            // This is the ID of the steer motor
-            FRONT_LEFT_MODULE_STEER_MOTOR,
-            // This is the ID of the steer encoder
-            FRONT_LEFT_MODULE_STEER_ENCODER,
-            // This is how much the steer encoder is offset from true zero (In our case,
-            // zero is facing straight forward)
-            FRONT_LEFT_MODULE_STEER_OFFSET);
-
-    // We will do the same for the other modules
-    frontRightModule =
-        Mk4SwerveModuleHelper.createFalcon500(
-            tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-                .withSize(2, 4)
-                .withPosition(2, 0),
-            Mk4SwerveModuleHelper.GearRatio.L1,
-            FRONT_RIGHT_MODULE_DRIVE_MOTOR,
-            FRONT_RIGHT_MODULE_STEER_MOTOR,
-            FRONT_RIGHT_MODULE_STEER_ENCODER,
-            FRONT_RIGHT_MODULE_STEER_OFFSET);
-
-    backLeftModule =
-        Mk4SwerveModuleHelper.createFalcon500(
-            tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-                .withSize(2, 4)
-                .withPosition(4, 0),
-            Mk4SwerveModuleHelper.GearRatio.L1,
-            BACK_LEFT_MODULE_DRIVE_MOTOR,
-            BACK_LEFT_MODULE_STEER_MOTOR,
-            BACK_LEFT_MODULE_STEER_ENCODER,
-            BACK_LEFT_MODULE_STEER_OFFSET);
-
-    backRightModule =
-        Mk4SwerveModuleHelper.createFalcon500(
-            tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-                .withSize(2, 4)
-                .withPosition(6, 0),
-            Mk4SwerveModuleHelper.GearRatio.L1,
-            BACK_RIGHT_MODULE_DRIVE_MOTOR,
-            BACK_RIGHT_MODULE_STEER_MOTOR,
-            BACK_RIGHT_MODULE_STEER_ENCODER,
-            BACK_RIGHT_MODULE_STEER_OFFSET);
+    frontLeftModule = createModule(DriveMap.FRONT_LEFT_MODULE, tab, 0, "Front Left Module");
+    frontRightModule = createModule(DriveMap.FRONT_RIGHT_MODULE, tab, 2, "Front Right Module");
+    backRightModule = createModule(DriveMap.BACK_RIGHT_MODULE, tab, 4, "Back Right Module");
+    backLeftModule = createModule(DriveMap.BACK_LEF_MODULE, tab, 6, "Back Left Module");
 
     swerveDriveOdometry = new SwerveDriveOdometry(kinematics, this.getGyroscopeRotation());
     setSwerveModuleState(chassisSpeeds);
@@ -192,7 +152,8 @@ public class SwerveDrive extends SubsystemBase {
   }
 
   /**
-   * Sets the gyroscope angle to zero. This can be used to set the direction the robot is currently
+   * Sets the gyroscope angle to zero. This can be used to set the direction the
+   * robot is currently
    * facing to the 'forwards' direction.
    */
   public void zeroGyroscope() {
